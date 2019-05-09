48fc5cd710d3b4ccad63fb6025094c8fd0811764
josesoengas <josesoengas@gmail.com>
2019-05-09 11:13:52 +0200

    Update get_top_painter.py
    I changed the code slightly.
    
    the output of the function is a dictionary with the top X painters. It has the same number of paintings for all artists based on the artist with least amount on paintings in the group

--- get_top_painters.py
+++ get_top_painters.py
@@ -7,25 +7,39 @@ dat = pd.read_csv('C:/Users/Jose Maria Soengas/Documents/siamese-CNN-master/trai
 img_dir = 'C:/Users/Jose Maria Soengas/Documents/siamese-CNN-master/train_1'
 
 
-image_groups = {}
-for index, row in dat.iterrows():
-    img_name = row['filename']
-    group_name = row['artist']
-    if group_name in image_groups:
-        image_groups[group_name].append(img_name)
-    else:
-        image_groups[group_name] = [img_name]
-
-csv_top = []
-
-for i in image_groups:
-   csv_top.append((i,len(image_groups[i])))
-
-csv_top = sorted(csv_top,key=lambda l:l[1], reverse=True)
-
-x = 3
-
+#x = number of artists you want
 def get_top(x):
-    return print(csv_top[:x])
+    #create dictionary of all artists and paintings
+    image_groups = {}
+    for index, row in dat.iterrows():
+        img_name = row['filename']
+        group_name = row['artist']
+        if group_name in image_groups:
+            image_groups[group_name].append(img_name)
+        else:
+            image_groups[group_name] = [img_name]
+    
+    csv_top = []
+    
+    #create an arracy of artists and the number of paintings they've drawn
+    for i in image_groups:
+       csv_top.append((i,len(image_groups[i])))
+    #sort the above array by ranking of number of paintings
+    csv_top = sorted(csv_top,key=lambda l:l[1], reverse=True)
+    
+    #define csv_top as list of top x artists
+    csv_top = csv_top[:x]
+    #determine the number of paintings of the painter with least amount of paintings in this top list
+    paintings_per_artist = (csv_top[x-1:x][0][1])
+    
+    #create a new dictionary of top painters with same number of paitings (paintings_per_artist) for all artists
+    top_image_groups = {}
+    for i in csv_top:
+        for k,v in image_groups.items():
+            if i[0] == k:
 
-get_top(x)
+                top_image_groups[k] = v
+    return top_image_groups
+
+get_top(2) 
+

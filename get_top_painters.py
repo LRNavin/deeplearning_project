f59433d675ee9a14d45b229630b55643316a4c4c
josesoengas <josesoengas@gmail.com>
2019-05-13 11:48:20 +0200

    added csv to folder

--- get_top_painters.py
+++ get_top_painters.py
@@ -18,20 +18,20 @@ def get_top(x):
             image_groups[group_name].append(img_name)
         else:
             image_groups[group_name] = [img_name]
-    
+
     csv_top = []
-    
+
     #create an arracy of artists and the number of paintings they've drawn
     for i in image_groups:
        csv_top.append((i,len(image_groups[i])))
     #sort the above array by ranking of number of paintings
     csv_top = sorted(csv_top,key=lambda l:l[1], reverse=True)
-    
+
     #define csv_top as list of top x artists
     csv_top = csv_top[:x]
     #determine the number of paintings of the painter with least amount of paintings in this top list
     paintings_per_artist = (csv_top[x-1:x][0][1])
-    
+
     #create a new dictionary of top painters with same number of paitings (paintings_per_artist) for all artists
     top_image_groups = {}
     for i in csv_top:
@@ -41,5 +41,5 @@ def get_top(x):
                 top_image_groups[k] = v
     return top_image_groups
 
-get_top(2) 
+get_top(2)
 
